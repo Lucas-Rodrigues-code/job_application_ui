14b56c89e326849a9a87a085ca4fa6a0c3953400
lucas.rodrigues <lucas.rodrigues@neocredit.com.br>
2024-11-19 20:09:46 -0300

    feat: add user registration functionality

--- api/auth.ts
+++ api/auth.ts
@@ -7,11 +7,34 @@ type PromiseSignIn = {
     email: string;
   };
 };
+
+type PromiseSignUp = {
+  id: string;
+  email: string;
+  password: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
 export async function AuthLogin(
   email: string,
   password: string
 ): Promise<PromiseSignIn> {
   const response = await api.post<PromiseSignIn>("/auth/sign-in", {
+    email,
+    password,
+  });
+  return response.data;
+}
+
+export async function AuthRegister(
+  name: string,
+  email: string,
+  password: string
+): Promise<PromiseSignUp> {
+  const response = await api.post<PromiseSignUp>("/user", {
+    name,
     email,
     password,
   });
